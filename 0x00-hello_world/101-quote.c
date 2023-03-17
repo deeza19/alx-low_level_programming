01f8c13768133e6164b5c78199d8a42b5f95bd53
deeza19 <zainabadisa425@gmail.com>
2023-03-17 16:10:32 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print quote
+ *
+ *
+ * Return: 1 (Successful)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015 - 10-19\n", stdio);
+	return (1);
+}
