35e31df4ceccb4b185615c7230b209721e99abaa
deeza19 <zainabadisa425@gmail.com>
2023-05-04 16:53:16 -0700

    bit

---
+++ 0x14-bit_manipulation/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned int z;
+		z = binary_to_uint("1");
+	printf("%u\n",z);
+
+	return (0);
+}
