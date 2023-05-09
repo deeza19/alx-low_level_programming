51f20b2768740387271eb476b08bad1510e0caad
deeza19 <zainabadisa425@gmail.com>
2023-05-09 03:05:06 -0700

    files

---
+++ 0x15-file_io/1-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0
+ */
+int main(int ac, char **ab)
+{
+	int dec;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", ab[0]);
+		exit(1);
+	}
+	dec = create_file(ab[1], ab[2]);
+	printf("-> %i\n", dec);
+	return (0);
+}
