c20cb1a638c5e84a4c5a74e700cccddd53fa5e6c
deeza19 <zainabadisa425@gmail.com>
2023-05-11 19:23:46 -0700

    files

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -23,21 +23,44 @@ void close_elf(int elf);
  *
  * Description: if the file is not an ELF file - exit code 98
  */
-void print_magic(unsigned char *e_indent);
+void check_elf(unsigned char *e_indent)
 {
 	int index;
 
-	printf(" Magic: ")
-
-		for (index = 0; index < EL_NIDENT; index++)
+		for (index = 0; index < 4; index++)
 		{
-			printf("%02x", e_indent[index]);
-
-			if (index == EI_NIDENT - 1)
-				printf("\n");
-			else 
-				printf(" ");
+			if (e_ident[index] != 127 &&
+					e_ident[index] != 'E' &&
+					e_ident[index] != 'L' &&
+					e_ident[index] != 'F')
+			{
+				dprintf(STDERR_FILENO, "Erro: Not an ELF file\n");
+				exit(98);
+			}
 		}
+}
+
+/**
+ * print_magic - prints the magic numbers of an ELF header
+ * @e_ident: a pointer to an array containing the ELF magic numbers
+ *
+ * Description: magic numbers are separated by paces
+ */
+void print_magic(unsigned char *e_ident)
+{
+	int index;
+
+	printf(" Magic: ");
+
+	for (index = 0; index < EI_INDENT; index++)
+	{
+		printf("%02x", e_ident[index]);
+
+		if (index == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
 }
 
 /**
@@ -140,6 +163,9 @@ void print_osabi(unsigned char *e_ident)
 			printf("UNIX - FreeBSD\n");
 			break;
 		case ELFOSABI_TRU64:
+			printf("UNIX - FreeBSD\n");
+			break;
+		case ELFOSABI_ARM:
 			printf("ARM\n");
 			break;
 		case ELFOSABI_STANDALONE:
@@ -210,7 +236,7 @@ void print_entry(unsigned long int e_entry, unsigned cah *e_ident)
 	if (e_ident[EI_CLASS] == ELFCLASS32)
 		printf("%#x\n", (unsigned int)e_entry);
 	else
-		printf("%#x\n", e_entry);
+		printf("%#lx\n", e_entry);
 }
 /**
  * close_elf - closes an ELF file
@@ -237,14 +263,14 @@ void close_elf(int elf)
  * Description: if  the file is not an ELF File or
  * the function fails - exit code 98
  */
-int main(int _attribute_((_unused_))arc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *heder;
 	int o, r;
 
 	o = open(argv[1], O_RDONLY);
 
-	IF (o == -1)
+	if (o == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
@@ -257,7 +283,16 @@ int main(int _attribute_((_unused_))arc, char *argv[])
 		exit(98);
 	}
 
-	check_elf(heade->e_ident);
+	r = read(o, header, sizeof(Elf64_Ehdr));
+	if (r == -1)
+	{
+		free(header);
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: %s : No such file\n", argv[1]);
+		exit(98);
+	}
+
+	check_elf(header->e_ident);
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
 	print_class(header->e_ident);
