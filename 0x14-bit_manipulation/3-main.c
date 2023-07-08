8a04ce285719af91ce41e4fe416d28342580e753
deeza_tb <zainabadisa425@gmail.com>
2023-07-08 16:17:50 +0100

    bits

---
+++ 0x14-bit_manipulation/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - checks the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int z;
+	z = 60;
+	set_bit(&z, 4);
+		printf("%lu\n", z);
+       return (0);
+}
