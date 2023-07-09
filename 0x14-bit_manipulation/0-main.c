1539f0ab1aa1203780deb7cd3407714983ab8ee5
deeza19 <zainabadisa425@gmail.com>
2023-07-09 03:28:52 -0700

    checked

--- 0x14-bit_manipulation/0-main.c
+++ 0x14-bit_manipulation/0-main.c
@@ -15,10 +15,10 @@ int main(void)
 	n = binary_to_uint("101");
 	printf("%u\n", n);
 	n = binary_to_uint("1e01");
-    printf("%u\n", n);
-    n = binary_to_uint("1100010");
-    printf("%u\n", n);
-    n = binary_to_uint("0000000000000000000110010010");
-    printf("%u\n", n);
-    return (0);
+	printf("%u\n", n);
+	n = binary_to_uint("1100010");
+	printf("%u\n", n);
+	n = binary_to_uint("0000000000000000000110010010");
+	printf("%u\n", n);
+	return (0);
 }
