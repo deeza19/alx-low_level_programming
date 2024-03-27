c22ce723598c0218a5c39ff7735a40e349381a03
deeza_tb <zainabadisa425@gmail.com>
2024-03-27 22:28:23 +0100

    putchar

--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,29 +1,13 @@
 #include "main.h"
+#include <unistd.h>
 /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: void
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriate
  */
-char *_strcat(char *dest, char *src)
+int _putchar(char c)
 {
-	int i;
-	int j;
-
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-
-	dest[i] = '\0';
-	return (dest);
+	return (write(1, &c, 1));
 }
